78fbb843eead834a36956e1a3932bc72a33ae39d
abdullahkhrisstoo <abd.abdullah2000@gmail.com>
2024-06-24 00:03:56 +0300

    add class 2add prop

--- Exam_Guardian.core/DTO/Class1.cs
+++ Exam_Guardian.core/DTO/Class1.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Exam_Guardian.core.DTO
 {
     internal class Class1
     {
+        public int MyProperty { get; set; }
     }
 }
